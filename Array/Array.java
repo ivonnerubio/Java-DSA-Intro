3ae01370a092acf8e05f25fc807d717a9d2544bc
ivonnerubio <ivonnerubiouh@gmail.com>
2021-11-15 19:45:08 -0600

    day one

---
+++ Array/Array.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] intArray = new int[7];
+
+        intArray[0] = 20;
+        intArray[1] = 21;
+        intArray[2] = 22;
+        intArray[3] = 24;
+        System.out.println(intArray);
+    }
+}
