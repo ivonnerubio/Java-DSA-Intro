359b6051d85e17df8f8c4bfb8a6556b7a966f7d5
ivonnerubio <ivonnerubiouh@gmail.com>
2022-01-22 07:59:16 -0600

    Merge Sort

---
+++ Sort_Algorithm/Merge_Sort/Merge_Sort.java
@@ -0,0 +1,45 @@
+package Sort_Algorithm.Merge_Sort;
+
+public class Merge_Sort {
+
+    public static void main(String[] args) {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+        mergeSort(intArray, 0, 7);
+
+    }
+
+
+    public static void mergeSort(int[] input, int start, int end){
+        if(end - start < 2){
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        mergeSort(input, start, mid);
+        mergeSort(input, mid, end);
+        merge(input, start, mid, end);
+        System.out.println("LOOK IM RUNNING");
+        for (int i =0; i<input.length; i++){
+            System.out.print(input[i] + ",");
+        }
+    }
+
+    public static void merge(int [] input, int start, int mid, int end){
+        if (input[mid-1] <= input[mid]){
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end - start];
+
+        while(i < mid && j < end){
+            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+        }
+
+        System.arraycopy(input, i, input, start + tempIndex, mid-i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
+    }
+}
