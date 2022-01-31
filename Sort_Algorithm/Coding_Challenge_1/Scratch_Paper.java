6b43feee3a9482cb90a687c12983279f2d584007
ivonnerubio <ivonnerubiouh@gmail.com>
2022-01-31 07:37:50 -0600

    Updated Coding Challenge 1

--- Sort_Algorithm/Coding_Challenge_1/Scratch_Paper.java
+++ Sort_Algorithm/Coding_Challenge_1/Scratch_Paper.java
@@ -2,16 +2,42 @@ package Sort_Algorithm.Coding_Challenge_1;
 
 public class Scratch_Paper {
     public static void main(String[] args) {
-        int start = 0;
-        int end = 3;
-        System.out.println((end-start)/2);
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
-        int i = 1;
-        int j = 2;
+        mergeSort(intArray, 0, intArray.length);
+
+        for (int i = 0; i < intArray.length; i++) {
+
+            System.out.println(intArray[i]);
+        }
+    }
+
+    public static void mergeSort(int[] input, int start, int end){
+        if(end-start <2){
+            return;
+        }
+
+        int mid = (start+end)/2;
+        mergeSort(input, start, mid);
+        mergeSort(input, mid, end);
+        merge(input, start, mid, end);
+
+    }
+
+    public static void merge(int[] input, int start,int mid,int end){
+        if(input[mid-1] >= input[mid]){
+            return;
+        }
+
+        int i = start;
+        int j =  mid;
         int tempIndex = 0;
+        int[] temp = new int[end-start];
 
-        int[] temp = new int[end - start];
-
-        //System.out.println(j);
+        while(i < mid && j < end){
+            temp[tempIndex++] = input[i] >= input[j] ? input[i++] : input[j++];
+        }
+        System.arraycopy(input, i, input, start + tempIndex, mid-i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
     }
 }
