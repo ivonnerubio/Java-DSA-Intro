c5c885b2688c2cb7951aa1d4f36c4ccac96476bc
ivonnerubio <ivonnerubiouh@gmail.com>
2022-02-04 16:53:56 -0600

    Completed Coding Challenge 3

--- Sort_Algorithm/Coding_Challenge_3/Coding_Challenge_3.java
+++ Sort_Algorithm/Coding_Challenge_3/Coding_Challenge_3.java
@@ -4,22 +4,51 @@ public class Coding_Challenge_3 {
     public static void main(String[] args) {
         String[] radixArray = { "bcdef", "dbaqc", "abcde", "omadd", "bbbbb"};
 
-        radixSort(radixArray, 10, 5);
+        radixSort(radixArray, 26, 5);
 
         for (int i = 0; i < radixArray.length; i++) {
             System.out.println(radixArray[i]);
         }
     }
 
-    public static void radixSort(String[] input, int radix, int width){
-        for (int i =0; i < width; i++){
+    public static void radixSort(String[] input, int radix, int width) {
+        for (int i = width-1; i >= 0; i--) {
             radixSingleSort(input, i, radix);
         }
     }
+        public static void radixSingleSort(String[] input, int position, int radix){
+            // 6 items
+            int numItems = input.length;
+            // new array with the length of 10
+            int[] countArray = new int[radix];
 
-    public static void radixSingleSort(String[] input, int position, int radix){
-        int numItems = input.length;
+            // for each value in the radix array
+            for(String value: input){
+                // assign the count array a value, it increments each time
+                // use the getDigit method, position = i (0-4), value =input.length , radix = 10
+                countArray[getIndex(position, value)]++;
+            }
 
-        int[] countArray = new int[radix];
-    }
+            // Adjust the count array
+            for(int j=1; j<radix; j++){
+                countArray[j] += countArray[j-1];
+            }
+
+            String[] temp = new String[numItems];
+
+            // temp[--countArray[getDigit(position,input[]
+            for(int tempIndex=numItems-1; tempIndex >=0; tempIndex--){
+                temp[--countArray[getIndex(position,input[tempIndex] )]] = input[tempIndex];
+            }
+
+            for(int tempIndex=0;tempIndex < numItems; tempIndex++){
+                input[tempIndex] = temp[tempIndex];
+            }
+
+
+        }
+
+        public static int getIndex(int position, String value){
+            return value.charAt(position) - 'a';
+        }
 }
