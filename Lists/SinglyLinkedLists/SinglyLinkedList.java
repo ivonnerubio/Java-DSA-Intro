7f2ec9c091b52a5065e97355ef30b2a21b70b691
ivonnerubio <ivonnerubiouh@gmail.com>
2022-02-06 09:55:50 -0600

    Added Linked List Implementation

---
+++ Lists/SinglyLinkedLists/SinglyLinkedList.java
@@ -0,0 +1,31 @@
+package Lists.SinglyLinkedLists;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SinglyLinkedList {
+
+    public static void main(String[] args) {
+
+        Employee janeDoe = new Employee("Jane", "Doe", 123);
+        Employee johnJones = new Employee("John", "Jones", 324);
+        Employee ivonneRubio = new Employee("ivonne", "rubio", 333);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 4344);
+
+        EmployeeLinkedList list = new EmployeeLinkedList();
+
+        System.out.println(list.isEmpty());
+
+        list.addToFront(janeDoe);
+        list.addToFront(johnJones);
+        list.addToFront(ivonneRubio);
+        list.addToFront(mikeWilson);
+
+        System.out.println(list.getSize());
+
+        list.printList();
+
+        list.removeFromFront();
+        System.out.println(list.getSize());
+        list.printList();
+    }
+}
