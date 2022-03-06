2bd9af1310ea74138494bfbb2aa4ade7e4907cbe
ivonnerubio <ivonnerubiouh@gmail.com>
2022-03-06 12:28:56 -0600

    Tree Case 1 and 2'

--- Trees/BinaryTrees/Tree.java
+++ Trees/BinaryTrees/Tree.java
@@ -20,6 +20,34 @@ public class Tree {
         return null;
     }
 
+    public void delete(int value){
+        root =  delete(root, value);
+    }
+
+    private TreeNode delete(TreeNode subtreeRoot, int value){
+        if(subtreeRoot == null){
+            return subtreeRoot;
+        }
+
+        if(value < subtreeRoot.getData()){
+            subtreeRoot.setLeftChild(delete(subtreeRoot.getLeftChild(),value));
+        }
+        else if(value > subtreeRoot.getData()){
+            subtreeRoot.setRightChild(delete(subtreeRoot.getRightChild(), value));
+        }
+        else{
+            // Cases 1 and 2: node to delete has 0 or 1 child(ren)
+            if(subtreeRoot.getLeftChild() == null){
+                return subtreeRoot.getRightChild();
+            }
+            else if(subtreeRoot.getRightChild() == null){
+                return subtreeRoot.getLeftChild();
+            }
+        }
+
+        return subtreeRoot;
+    }
+
     public int min(){
         if(root == null){
             return Integer.MIN_VALUE;
