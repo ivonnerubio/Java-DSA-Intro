0fa66ad1aee63dec3953268adb5778a47857c02d
ivonnerubio <ivonnerubiouh@gmail.com>
2022-03-06 10:34:41 -0600

    Added min max tree

---
+++ Trees/BinaryTrees/Main.java
@@ -0,0 +1,24 @@
+package Trees.BinaryTrees;
+
+public class Main {
+    public static void main(String[] args) {
+        Tree intTree = new Tree();
+        intTree.insert(25);
+        intTree.insert(20);
+        intTree.insert(15);
+        intTree.insert(30);
+        intTree.insert(29);
+        intTree.insert(26);
+        intTree.insert(22);
+        intTree.insert(32);
+
+       // intTree.traverseInOrder();
+
+        System.out.println(intTree.get(26));
+        System.out.println(intTree.get(17));
+        System.out.println(intTree.get(888));
+
+        System.out.println(intTree.min());
+        System.out.println(intTree.max());
+    }
+}
