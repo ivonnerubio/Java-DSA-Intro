6d78e22f78951dbee9d1711772f0aea4b933c739
ivonnerubio <ivonnerubiouh@gmail.com>
2022-03-11 07:44:47 -0600

    Added Insertion Theory

--- Sort_Algorithm/Practice/Insertion_Sort/Main.java
+++ Sort_Algorithm/Practice/Insertion_Sort/Main.java
@@ -6,18 +6,15 @@ public class Main {
     public static void main(String[] args) {
         int arr[] = {12, 11, 13, 5, 6};
 
-        for(int unsorted=1; unsorted<arr.length; unsorted++){
+        for(int unsorted = 1; unsorted < arr.length; unsorted++){
             int new_element = arr[unsorted];
-            int i =0;
+            int i;
 
-            for(i =0; i > 0 && arr[i-1] > new_element; i--){
+            for(i=unsorted; i > 0 && arr[i-1] > new_element; i--){
                 arr[i] = arr[i-1];
             }
             arr[i] = new_element;
-
-
         }
-
         for(int i =0; i<arr.length;i++){
             System.out.println(arr[i]);
         }
